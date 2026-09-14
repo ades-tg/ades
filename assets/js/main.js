a89fb2edff9ac1086063b08d4c9fab10dd9925ca
anadeb <codesource_transfert@anadeb.tg>
2026-09-14 11:30:28 +0000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+const toggle=document.querySelector('.menu-toggle');const nav=document.querySelector('#main-nav');toggle?.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('#main-nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));document.querySelector('#year').textContent=new Date().getFullYear();
